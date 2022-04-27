7a8ffbc43c4ff3128da9fae0b75873d9c7cf5a88
Karim <karim.summary@gmail.com>
2022-04-27 21:45:37 +0300

    hib1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,45 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+ //   private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/test?serverTimezone=Europe/Moscow";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "MyS@12348765";
+    private static SessionFactory sessionFactory = null;
+
+    public static SessionFactory getConnection() {
+
+        try {
+            Configuration configuration = new Configuration()
+                   // .setProperty("hibernate.connection.driver_class", DRIVER)
+                    .setProperty("hibernate.connection.url", URL)
+                    .setProperty("hibernate.connection.username", LOGIN)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .addAnnotatedClass(User.class)
+                    .setProperty("hibernate.c3p0.min_size","5")
+                    .setProperty("hibernate.c3p0.max_size","200")
+                    .setProperty("hibernate.c3p0.max_statements","200");
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
+    }
+    public static void closeConnection() {
+        if (sessionFactory != null)
+            sessionFactory.close();
+    }
 }
+    // реализуйте настройку соеденения с БД
+
